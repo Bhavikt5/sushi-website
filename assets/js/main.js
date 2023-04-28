144f8e0dbd5a651a330cfe50d0b119ba810b52b3
bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2023-04-28 21:00:03 +0530

    made landing page with responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,25 @@
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".fa-bars");
+const darkmode = document.querySelector(".fa-moon");
+const body = document.body;
 /*=============== SHOW MENU ===============*/
-
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+menu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+darkmode.addEventListener("click", () => {
+  body.classList.toggle("darkmode");
+});
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
